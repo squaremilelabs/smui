fd7f97267a61472a242c2d781c99a71f90fd1da9
E <e@elzr.me>
2025-07-21 20:55:13 -0400

    icon variants

--- icon/variants.ts
+++ icon/variants.ts
@@ -2,13 +2,15 @@ import { tv, VariantProps } from "../utils"
 
 export type IconVariantProps = VariantProps<typeof iconVariants>
 export const iconVariants = tv({
-  base: [],
+  base: ["flex items-center justify-center"],
   variants: {
-    variant: {
-      default: [],
+    size: {
+      sm: "size-16 min-w-16 min-h-16 [&_svg]:!size-12",
+      md: "size-20 min-w-20 min-h-20 [&_svg]:!size-16",
+      lg: "size-24 min-w-24 min-h-24 [&_svg]:!size-20",
     },
   },
   defaultVariants: {
-    variant: "default",
+    size: "md",
   },
 })
