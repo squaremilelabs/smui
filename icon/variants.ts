a910d468968d1faa9278e0d1f6cb142ea2cc33db
E <e@elzr.me>
2025-07-26 16:18:06 -0400

    update icon

--- icon/variants.ts
+++ icon/variants.ts
@@ -5,9 +5,9 @@ export const iconVariants = tv({
   base: ["flex items-center justify-center"],
   variants: {
     size: {
-      sm: "size-16 min-w-16 min-h-16 [&_svg]:!size-12",
-      md: "size-20 min-w-20 min-h-20 [&_svg]:!size-16",
-      lg: "size-24 min-w-24 min-h-24 [&_svg]:!size-20",
+      sm: "size-16 min-w-16 min-h-16 [&_svg]:!size-12 [&_img]:!size-12",
+      md: "size-20 min-w-20 min-h-20 [&_svg]:!size-16 [&_img]:!size-16",
+      lg: "size-24 min-w-24 min-h-24 [&_svg]:!size-20 [&_img]:!size-20",
     },
   },
   defaultVariants: {
