41580f60b4a81e5289378a1328cf208e0c9f9ce4
E <e@elzr.me>
2025-09-19 15:00:59 -0400

    remove

--- index.ts
+++
@@ -1,5 +0,0 @@
-export { tv, twm } from "./utils/tailwind"
-export type { SlottedClassNames, VariantProps, ClassValue } from "./utils/tailwind"
-
-export { SMUIDialog, SMUIDialogTrigger } from "./components/dialog"
-export type { SMUIDialogProps } from "./components/dialog"
